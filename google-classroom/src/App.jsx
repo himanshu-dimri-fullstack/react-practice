07e691f71497f4483e3b189974035cb1f9f5d1b7
Himanshu Dimri <himanshudimrifullstack@gmail.com>
2026-04-13 20:34:32 +0530

    Add card layout

---
+++ google-classroom/src/App.jsx
@@ -0,0 +1,104 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='main'>
+        <div className='sidebar'></div>
+        <div className='card-container'>
+          <div className='card'>
+            <div className='card-head'>
+              <div className='head'><a href="#">Frontend Batch - N</a></div>
+              <div className='tag'><a href="#">Regex Software</a></div>
+              <div className='profile-img'>
+                <img src="https://avatars.githubusercontent.com/u/256261357?v=4" />
+              </div>
+            </div>
+            <div className='card-content'></div>
+            <div className='card-footer'></div>
+          </div>
+          <div className='card'>
+            <div className='card-head'>
+              <div className='head'><a href="#">Frontend Batch - N</a></div>
+              <div className='tag'><a href="#">Regex Software</a></div>
+              <div className='profile-img'>
+                <img src="https://avatars.githubusercontent.com/u/256261357?v=4" />
+              </div>
+            </div>
+            <div className='card-content'></div>
+            <div className='card-footer'></div>
+          </div>
+          <div className='card'>
+            <div className='card-head'>
+              <div className='head'><a href="#">Frontend Batch - N</a></div>
+              <div className='tag'><a href="#">Regex Software</a></div>
+              <div className='profile-img'>
+                <img src="https://avatars.githubusercontent.com/u/256261357?v=4" />
+              </div>
+            </div>
+            <div className='card-content'></div>
+            <div className='card-footer'></div>
+          </div>
+          <div className='card'>
+            <div className='card-head'>
+              <div className='head'><a href="#">Frontend Batch - N</a></div>
+              <div className='tag'><a href="#">Regex Software</a></div>
+              <div className='profile-img'>
+                <img src="https://avatars.githubusercontent.com/u/256261357?v=4" />
+              </div>
+            </div>
+            <div className='card-content'></div>
+            <div className='card-footer'></div>
+          </div>
+          <div className='card'>
+            <div className='card-head'>
+              <div className='head'><a href="#">Frontend Batch - N</a></div>
+              <div className='tag'><a href="#">Regex Software</a></div>
+              <div className='profile-img'>
+                <img src="https://avatars.githubusercontent.com/u/256261357?v=4" />
+              </div>
+            </div>
+            <div className='card-content'></div>
+            <div className='card-footer'></div>
+          </div>
+          <div className='card'>
+            <div className='card-head'>
+              <div className='head'><a href="#">Frontend Batch - N</a></div>
+              <div className='tag'><a href="#">Regex Software</a></div>
+              <div className='profile-img'>
+                <img src="https://avatars.githubusercontent.com/u/256261357?v=4" />
+              </div>
+            </div>
+            <div className='card-content'></div>
+            <div className='card-footer'></div>
+          </div>
+          <div className='card'>
+            <div className='card-head'>
+              <div className='head'><a href="#">Frontend Batch - N</a></div>
+              <div className='tag'><a href="#">Regex Software</a></div>
+              <div className='profile-img'>
+                <img src="https://avatars.githubusercontent.com/u/256261357?v=4" />
+              </div>
+            </div>
+            <div className='card-content'></div>
+            <div className='card-footer'></div>
+          </div>
+          <div className='card'>
+            <div className='card-head'>
+              <div className='head'><a href="#">Frontend Batch - N</a></div>
+              <div className='tag'><a href="#">Regex Software</a></div>
+              <div className='profile-img'>
+                <img src="https://avatars.githubusercontent.com/u/256261357?v=4" />
+              </div>
+            </div>
+            <div className='card-content'></div>
+            <div className='card-footer'></div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
