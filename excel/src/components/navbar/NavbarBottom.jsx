b443c2de9e9b01ec51a291e4cff1883b787b92bd
Himanshu Dimri <himanshudimrifullstack@gmail.com>
2026-04-19 23:22:00 +0530

    Add navbar bottom

--- excel/src/components/navbar/NavbarBottom.jsx
+++ excel/src/components/navbar/NavbarBottom.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import { Clipboard } from "lucide-react";
+import { Scissors } from "lucide-react";
+import { Copy } from "lucide-react";
+import { Paintbrush } from "lucide-react";
+import { ClipboardPaste } from "lucide-react";
+
 
 const NavbarBottom = () => {
     return (
         <div>
+            {/* clipboard */}
+            <div className='clipboard'>
+                <div>
+                    <ClipboardPaste />
+                </div>
+                <div>
+                    <Copy />
+                    <Scissors />
+                </div>
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
 
         </div>
     )
